1a1d30bc12ab42bbf16a1881c6111b3c580690cf
akter037 <94971719+akter037@users.noreply.github.com>
2022-11-13 20:03:11 +0600

    photo gallery web

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './header.css'
+import Nav from './nav/Nav'
+
+const Header = () => {
+  return (
+    <div id='#' className='header_bg'>
+      <Nav></Nav>
+        <div className="container header_container">
+           <h2>JONATHON KUBB</h2>
+           <h3>PHOTO GRAPHY</h3>
+           <p>Sub Title</p>
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/header/nav/Nav.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './nav.css'
+import Logo from '../../../assets/logo.png'
+import {useState} from 'react'
+import {ImMenu} from 'react-icons/im'
+import {ImCross} from "react-icons/im";
+
+
+const Nav = () => {
+  const [activeNav,setActiveNav]=useState('#')
+  const [isMobile,setIsMobile]=useState(false)
+
+  return (
+    <div id='nav' className='bg_nav'>
+      
+      <div className="container nav_container">
+       
+       <div className="nav_logo">
+            <img src={Logo} alt="" />
+            <h2>Kubb</h2>
+       </div>
+       
+       <div className={isMobile? "pc_display" : "mb_display"} onClick={()=> setIsMobile(false)}>
+
+        <ul>
+          <li><a href="#" onClick={()=> setActiveNav('#')} className={activeNav=== '#'? 'active':' '}>Home</a></li>
+          <li><a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav=== '#about'? 'active':' '}>About</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#services" onClick={()=> setActiveNav('#services')} className={activeNav=== '#services'? 'active':' '}>Services</a></li>
+          <li><a href="#testimonials" onClick={()=> setActiveNav('#testimonials')} className={activeNav=== '#testimonials'? 'active':' '}>Journal</a></li>
+          <li><a href="#contact"  onClick={()=> setActiveNav('#contact')} className={activeNav=== '#contact'? 'active':' '}>Contact</a></li>
+          
+        </ul>
+       </div>
+       <button  className='mobile-menu-icon' 
+      onClick={() => setIsMobile(!isMobile)}>
+    {isMobile ?(<ImCross size={20}/>
+    
+    ):(
+    <ImMenu size={25}/>
+    )}
+     </button>
+
+
+      </div>
+
+
+
+    </div>
+  )
+}
+
+export default Nav
